4df4179d854e5b9cf4bfe165aea01c7a669ea3af
Albert Szymański <alb.szym1@gmail.com>
2024-10-14 15:31:23 +0200

    feat: added cors, docker compose and appsettings.Development to repos

---
+++ SimpleDotNetStarter/Common/Cors/CorsOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleDotNetStarter.Common.Cors;
+
+internal sealed class CorsOptions
+{
+    public const string SectionName = "Cors";
+
+    public string[] AllowedOrigins { get; set; }
+}
